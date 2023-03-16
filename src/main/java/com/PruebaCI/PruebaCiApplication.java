96c0007b7d1581ed6116ec701ecea5e231d4a725
Evelin Aragon <eve_aragon@hotmail.com>
2023-03-16 14:26:30 -0300

    subiendo

---
+++ src/main/java/com/PruebaCI/PruebaCiApplication.java
@@ -0,0 +1,13 @@
+package com.PruebaCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PruebaCI/PruebaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PruebaCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
